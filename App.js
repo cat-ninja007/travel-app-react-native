e7293d2a5a3552b7c682774b423c1ee306f6ecc0
cat-ninja007 <archiedesign12.2@gmail.com>
2024-10-14 11:04:55 +0700

    revert commit

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import MainNavigator from './src/navigator/MainNavigator'
-import TryCodeScreen from './src/screens/TryCodeScreen'
-// import HelloScreen from './src/screens/HelloScreen'
+import HelloScreen from './src/screens/HelloScreen'
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      {/* <MainNavigator /> */}
-      <TryCodeScreen />
+      <HelloScreen />
     </SafeAreaProvider>
   )
 }
